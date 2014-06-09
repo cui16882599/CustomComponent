e2db1c3fb542b9c73761a8352782683940ba71ef
cuiqing <cuiqing@360.cn>
2014-06-09 16:16:57 +0800

    delete gen and bin

--- src/com/example/customcomponent/MainActivity.java
+++ src/com/example/customcomponent/MainActivity.java
@@ -31,12 +31,6 @@ public class MainActivity extends Activity implements OnClickListener{
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
     public void onClick(View v) {
         switch (v.getId()) {
         case R.id.btn_total:
